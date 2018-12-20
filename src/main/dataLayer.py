c9e63f6383abedc679204986b833d9dec28ff5ff
gvsans1 <41053519+gvsans1@users.noreply.github.com>
2018-12-20 13:25:21 +0100

    Update dataLayer.py

--- src/main/dataLayer.py
+++ src/main/dataLayer.py
@@ -1,52 +1,35 @@
-# -*- coding: utf-8 -*-
-
-
-"""
-This module reads from a gz file stored in data, containing information on 
-Amazon Reviews.
-The module first reads the gz file and extracts a number of reviews - 
-currently 1000.
-The module then reads and store the extracts in memory for use by the main.py.
-"""
-
-# %%
-'''Import Test Data'''
-
+"""Import libraries"""
 import pandas as pd
 import gzip
 
+"""
+This module reads from a gz file stored in data, containing information on
+Amazon Reviews.
 
-def importNlpTestData(numberOfReviews):
+    * The module first reads the gz file and extracts 
+      a number of reviews.
+    * The module then reads and store the extracts in memory
+      for use by the main.py.
+"""
+
+
+def import_nlp_test_data(number_of_reviews):
     def parse(path):
         g = gzip.open(path, 'rb')
         for l in g:
             yield eval(l)
 
-    def getDF(path):
+    def get_dataframe(path):
         i = 0
-        df = {}
+        dataframe = {}
         for d in parse(path):
-            df[i] = d
+            dataframe[i] = d
             i += 1
-        return pd.DataFrame.from_dict(df, orient='index')
+        return pd.DataFrame.from_dict(dataframe, orient='index')
 
     pth = r"data\reviews_Musical_Instruments_5.json.gz"
-    df = getDF(pth)
+    df = get_dataframe(pth)
 
-    df_test = df[df.index < numberOfReviews]
+    df_test = df[df.index < number_of_reviews]
 
     df_test.to_csv("nlpTestData.csv", sep="\t")
-
-
-# %%
-'''Read Test Data'''
-
-
-def readTestData():
-    pth = r'data/nlpTestData.csv'
-    testData = pd.read_csv(pth, sep="\t", engine='python')
-    return testData
-
-
-# %%
-print("Data Layer succesfully in place")
