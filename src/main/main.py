b2eb38b409b1d867c032e9abaf17939a4da6529d
gvsans1 <41053519+gvsans1@users.noreply.github.com>
2018-12-20 13:25:05 +0100

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -1,144 +1,100 @@
-# -*- coding: utf-8 -*-
-
-# %%
 """
 Import Modules
 """
 import pandas as pd
 import statsmodels.api as sm
-import customTokenizer
-import machineLearningModule
-import dataLayer
+from src.main.data_layer import import_nlp_test_data
+from src.main.custom_tokenizer import custom_tokenizer_vector
+from src.main.document_term_matrix_reduction import reduce_document_term_matrix
+from src.main.machine_learning_module import (get_document_term_matrix,
+                                              get_mnb_score)
 
 # %%
 """
-Get Data
+1. Get Test Data
 """
 
-dataLayer.importNlpTestData(10500)
-testData = dataLayer.readTestData()
-
-# %%
-"""
-Run - Get vector of tokens (only as a test)
-"""
-tokens = \
-    customTokenizer.customTokenizerVector(
-        testData['reviewText']
-    )
+import_nlp_test_data(10500)
+pth = r'data/nlpTestData.csv'
+test_data = pd.read_csv(pth, sep="\t")
 
 # %%
 """
-Run - Get fit score for all methodologies:
-  * Freqnecy measure: Count vs TFID
+2. Run - Get vector of tokens (only as a test)
+"""
+tokens = custom_tokenizer_vector(test_data['reviewText'])
+
+# %%
+"""
+3. Run - Get fit score for all methodologies:
+  * Frequency measure: Count vs TFID
   * Tokenizer: custom vs standard
 
 """
-# for i in [False, True]:
-#     for j in [False, True]:
-#         machineLearningModule.getMnbScore(
-#           x=testData['reviewText'],
-#           y=testData['overall'], TFID=i,
-#           customToken=j)
-
 
 # Count Vectorizer and Standard Tokenizer
-machineLearningModule.getMnbScore(
-    x=testData['reviewText'],
-    y=testData['overall'], TFID=False,
-    customToken=False)
+get_mnb_score(
+    x=test_data['reviewText'],
+    y=test_data['overall'], tfid=False,
+    custom_token=False)
 
 # TFID and Standard Tokenizer
-machineLearningModule.getMnbScore(
-    x=testData['reviewText'],
-    y=testData['overall'], TFID=True,
-    customToken=False)
+get_mnb_score(
+    x=test_data['reviewText'],
+    y=test_data['overall'], tfid=True,
+    custom_token=False)
 
 # Count Vectorizer and Custom Tokenizer
-machineLearningModule.getMnbScore(
-    x=testData['reviewText'],
-    y=testData['overall'], TFID=False,
-    customToken=True)
+get_mnb_score(
+    x=test_data['reviewText'],
+    y=test_data['overall'], tfid=False,
+    custom_token=True)
 
-#TFID and Custom Tokenizer
-machineLearningModule.getMnbScore(
-    x=testData['reviewText'],
-    y=testData['overall'], TFID=True,
-    customToken=True)
-
-
-# %%
-"""
-Run - Obtain term-document matrix for best-fit model.
-Let's use CV with our custom tokenizer
-"""
-
-cvCustom = \
-    machineLearningModule.getDocumentTermMatrix(
-        x=testData['reviewText'],
-        TFID=0, customToken=1)
-
-cvStd = \
-    machineLearningModule.getDocumentTermMatrix(
-        x = testData['reviewText'],
-        TFID = 0, customToken = 0)
-
-tfidCustom = \
-    machineLearningModule.getDocumentTermMatrix(
-        x = testData['reviewText'],
-        TFID = 1, customToken = 1)
-
-tfidStd = \
-    machineLearningModule.getDocumentTermMatrix(
-        x = testData['reviewText'],
-        TFID = 1, customToken = 0)
+# TFID and Custom Tokenizer
+get_mnb_score(
+    x=test_data['reviewText'],
+    y=test_data['overall'], tfid=True,
+    custom_token=True)
 
 # %%
 """
-Keep only columns (i.e. tokens) where the TFID sum value is above k-th
-percentile or k threshold
+4. Run - Obtain term-document matrix for best-fit model.
+   * Use CV with our custom tokenizer
+   * Reduce resulting document-term matrix filtering out low-importance tokens
 """
 
-def reduceDocumentTermMatrix(chosenMatrix, method, k):
-    sumScore = chosenMatrix.sum(axis=0)
-    if method == 'percentile':
-        topTerms = sumScore[sumScore > sumScore.quantile(k)]
-    elif method == 'max':
-        topTerms = sumScore.nlargest(k)
-    topTermsList = topTerms.index.tolist()
-    out = chosenMatrix[topTermsList]
-    return out
+# Get document-term matrix using count vectorizer and our custom tokenizer
+cv_custom = \
+    get_document_term_matrix(
+        data=test_data['reviewText'],
+        tfid=False,
+        custom_token=True)
 
+# Keep only tokens above a certain percentile
 dataTop = \
-    reduceDocumentTermMatrix(
-        chosenMatrix=cvCustom,
+    reduce_document_term_matrix(
+        chosen_matrix=cv_custom,
         method='percentile',
         k=0.999)
 
-#  dataTop = \
-#   reduceDocumentTermMatrix(
-#     chosenMatrix = cvCustom,
-#     method = 'max',
-#     k = 10)
-
 # %%
 """
-Create dataset for regression
+5. Create dataset for regression
 """
 regressionData = \
     pd.merge(
         dataTop,
-        pd.DataFrame(testData['overall']),
+        pd.DataFrame(test_data['overall']),
         how='left',
         left_index=True,
         right_index=True)
 
 # %%
 """
-Fit Multinomial Logit
+6. Fit a Multinomial Logit
 """
-X = regressionData[dataTop.columns]
+x = regressionData[dataTop.columns]
 y = regressionData.overall
-logitModel = sm.MNLogit(y, X)
-result = logitModel.fit()
+logit_model = sm.MNLogit(y, x)
+result = logit_model.fit()
 print(result.summary())
